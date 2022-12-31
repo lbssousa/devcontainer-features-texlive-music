ae25a8690afcc850e14e5072e41faab4cf63e4f4
Laércio de Sousa <lbsousajr@gmail.com>
2022-12-31 14:55:09 -0300

    Update LilyPond install script reflecting new package structure.

--- src/lilypond/install.sh
+++ src/lilypond/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-LILYPOND_VERSION=${VERSION:-2.24.0}-1
-LILYPOND_INSTALLER=lilypond-${LILYPOND_VERSION}.linux-64.sh
+LILYPOND_VERSION=${VERSION:-2.24.0}
+LILYPOND_PACKAGE=lilypond-${LILYPOND_VERSION}.linux-x86_64.tar.gz
 
 # Taken from https://github.com/devcontainers/features/blob/main/src/go/install.sh
 apt_get_update()
@@ -25,14 +25,20 @@ check_packages() {
 
 check_packages curl ca-certificates bzip2
 
-echo "Downloading LilyPond installer, version ${LILYPOND_VERSION}..."
-curl -O https://lilypond.org/download/binaries/linux-64/${LILYPOND_INSTALLER}
+echo "Downloading LilyPond version, version ${LILYPOND_VERSION}..."
+curl -O https://gitlab.com/lilypond/lilypond/-/releases/v${LILYPOND_VERSION}/downloads/${LILYPOND_PACKAGE}
 
-echo "Running LilyPond installer..."
-sh ./${LILYPOND_INSTALLER} --batch
+echo "Installing LilyPond..."
+tar xzvf ${LILYPOND_PACKAGE} -C /opt
+
+for bin in /opt/lilypond-${LILYPOND_VERSION}/bin
+do
+    ln -s ${bin} /usr/local/bin
+done
+
 
 # Clean up
-rm -f ./${LILYPOND_INSTALLER}
-rm -rf /var/lib/apt/lists/*
+rm -f ./${LILYPOND_PACKAGE}
+apt-get clean
 
 echo "Done!"
