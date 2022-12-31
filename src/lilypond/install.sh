fc87cfebd82f97bd8edfffee5058747301077513
Laércio de Sousa <lbsousajr@gmail.com>
2022-12-31 17:26:51 -0300

    Fix lilypond package download in install script.

--- src/lilypond/install.sh
+++ src/lilypond/install.sh
@@ -3,6 +3,7 @@ set -e
 
 LILYPOND_VERSION=${VERSION:-2.24.0}
 LILYPOND_PACKAGE=lilypond-${LILYPOND_VERSION}-linux-x86_64.tar.gz
+LILYPOND_SHA256=5ed6ced6ade894b6dc1db35474b5e82a175a1149d34f16e75aaa9d885d9d3f6a
 
 # Taken from https://github.com/devcontainers/features/blob/main/src/go/install.sh
 apt_get_update()
@@ -26,7 +27,13 @@ check_packages() {
 check_packages curl ca-certificates bzip2
 
 echo "Downloading LilyPond version, version ${LILYPOND_VERSION}..."
-curl -O https://gitlab.com/lilypond/lilypond/-/releases/v${LILYPOND_VERSION}/downloads/${LILYPOND_PACKAGE}
+curl -LO https://gitlab.com/lilypond/lilypond/-/releases/v${LILYPOND_VERSION}/downloads/${LILYPOND_PACKAGE}
+
+if [ $(sha256sum ${LILYPOND_PACKAGE} | cut -d' ' -f1) != ${LILYPOND_SHA256} ]
+then
+   "LilyPond package SHA-256 checksum failed."
+   exit 1
+fi
 
 echo "Installing ${LILYPOND_PACKAGE}..."
 tar xzvf ${LILYPOND_PACKAGE} -C /opt
