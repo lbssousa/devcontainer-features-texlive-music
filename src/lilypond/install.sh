ef2422a3655a8b316d143acec6e93e9fe0a84ca7
Laércio de Sousa <lbsousajr@gmail.com>
2023-08-14 11:42:48 -0300

    Install ghostscript package (needed for lilypond/lyluatex).

--- src/lilypond/install.sh
+++ src/lilypond/install.sh
@@ -4,26 +4,57 @@ set -e
 LILYPOND_VERSION=${VERSION:-2.24.1}
 LILYPOND_PACKAGE=lilypond-${LILYPOND_VERSION}-linux-x86_64.tar.gz
 
-# Taken from https://github.com/devcontainers/features/blob/main/src/go/install.sh
-apt_get_update()
-{
-    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]
-    then
-        echo "Running apt-get update..."
-        apt-get update -y
-    fi
+install_debian_packages() {
+    echo "Running apt-get update..."
+    apt-get update -y
+
+    echo "Installing packages: ${@}"
+    apt-get -y install --no-install-recommends "${@}"
 }
 
-# Checks if packages are installed and installs them if not
-check_packages() {
-    if ! dpkg -s "${@}" > /dev/null 2>&1
-    then
-        apt_get_update
-        apt-get -y install --no-install-recommends "${@}"
-    fi
+install_alpine_packages() {
+    apk update
+    apk add --no-cache "${@}"
 }
 
-check_packages curl ca-certificates bzip2 unzip
+install_alpine_community_packages() {
+    apk add --no-cache --repository=https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community "${@}"
+}
+
+install_alpine_testing_packages() {
+    apk add --no-cache --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing "${@}"
+}
+
+# Bring in ID, ID_LIKE, VERSION_ID, VERSION_CODENAME
+. /etc/os-release
+# Get an adjusted ID independant of distro variants
+if [ "${ID}" = "debian" ] || [ "${ID_LIKE}" = "debian" ]
+then
+    ADJUSTED_ID="debian"
+elif [[ "${ID}" = "rhel" || "${ID}" = "fedora" || "${ID}" = "mariner" || "${ID_LIKE}" = *"rhel"* || "${ID_LIKE}" = *"fedora"* || "${ID_LIKE}" = *"mariner"* ]]
+then
+    ADJUSTED_ID="rhel"
+elif [ "${ID}" = "alpine" ]
+then
+    ADJUSTED_ID="alpine"
+else
+    echo "Linux distro ${ID} not supported."
+    exit 1
+fi
+
+case "${ADJUSTED_ID}" in
+debian)
+    install_debian_packages ca-certificates libfile-homedir-perl libunicode-linebreak-perl libyaml-tiny-perl perl-doc wget
+    ;;
+alpine)
+    install_alpine_packages ca-certificates lilypond
+    ;;
+rhel)
+    echo "RPM-based Linux distros not yet supported."
+    exit 1
+esac
+
+check_packages curl ca-certificates bzip2 unzip ghostscript
 
 echo "Downloading EB Garamond font..."
 curl -L https://github.com/octaviopardo/EBGaramond12/archive/refs/heads/master.zip -o ebgaramond.zip
